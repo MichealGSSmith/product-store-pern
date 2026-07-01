cc3056cbe2d97933c5ebf6468df7e143f2b35924
MrCadillac <m.gordon.smith@hotmail.com>
2026-07-01 14:17:20 -0400

    made the add a product
    created the add a product button functionality and added test data to make sure it went to DB

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useEffect} from 'react'
 import { useProductStore } from '../store/useProductStore'
-import {PlusCircleIcon, RefreshCwIcon} from "lucide-react"
+import {PackageIcon, PlusCircleIcon, RefreshCwIcon} from "lucide-react"
 import ProductCard from "../components/ProductCard"
+import AddProductModal from '../components/AddProductModal'
+
+
 function HomePage() {
   const { products, loading, error, fetchProducts} = useProductStore();
 
@@ -14,7 +17,7 @@ function HomePage() {
   return (
     <main className="max-w-6xl mx-auto px-4 py-8">
       <div className=" flex justify-between items-center mb-8">
-        <button className= " btn btn-primary">
+        <button className= " btn btn-primary" onClick={() => document.getElementById('add_product_modal').showModal()}>
           <PlusCircleIcon className= "size-5 mr-2" />
           Add Product
         </button>
@@ -23,7 +26,22 @@ function HomePage() {
         </button>
       </div>
 
+      <AddProductModal />
+
       {error && <div className='alert alert-error mb-8'>{error}</div>}
+
+      {products.length === 0 && !loading && (
+        <div className='flex flex-col justify-center items-center h-96 space-y-4'>
+          <div className='bg-base-100 rounded-full p-6'>
+            <PackageIcon className='size-12' />
+          </div>
+            <div className='text-center space-y-2'>
+              <h3 className='text-2xl font-semibold'> No Products found</h3>
+              <p className='text-gray-500 max-w-sm'>Get started by adding your first product to your inventory page</p>
+            </div>
+        
+        </div>
+      )}
 
       {loading ? (
         <div className='flex justify-center items-center h-64'>
--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -5,9 +5,39 @@ import toast from 'react-hot-toast';
 const BASE_URL = "http://localhost:3000";
 
 export const useProductStore = create((set, get) => ({
+    //product state
     products:[],
     loading: false,
     error:null,
+
+    //Form state
+    formData: {
+        name:"",
+        price:"",
+        image:"",
+    },
+    setFormData: (formData) => set ({ formData }),
+    resetForm: () => set({ formData: { name:"", price:"", image:""}}),
+
+    addProduct: async(e) => {
+        e.preventDefault();
+        set({ loading: true});
+
+        try {
+            const {formData} = get()
+            await axios.post(`${BASE_URL}/api/products`, formData)
+            await get().fetchProducts();
+            get().resetForm();
+            toast.success("Product added successfully");
+            
+        } catch (error) {
+            console.log("Error in the addProduct function", error);
+            toast.error("Something went wrong, oops!");
+        } finally {
+            set({loading: false});
+        }
+    },
+
     fetchProducts: async () => {
         set({loading: true});
         try{
